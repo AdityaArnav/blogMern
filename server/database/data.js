e4a9a5c010846941f9a7ae7b6bcc4e0fa6336007
AdityaArnav <99977930+AdityaArnav@users.noreply.github.com>
2022-07-02 23:15:45 +0530

    Revert "added a lil bit backend"
    This reverts commit 4c13e8d9c59df97bd6d132d085651aa37d44a7be.

--- server/database/data.js
+++ server/database/data.js
@@ -1,43 +1,19 @@
 
-// import mongoose from 'mongoose'
+import mongoose from 'mongoose'
 
-// const Connection = async(username,password) => {
-
-//     // VQ88KVnBicpJDbEQ
-//     const url = `mongodb://${username}:${password}@blog-website-shard-00-00.valyh.mongodb.net:27017,blog-website-shard-00-01.valyh.mongodb.net:27017,blog-website-shard-00-02.valyh.mongodb.net:27017/?ssl=true&replicaSet=atlas-2gwitb-shard-0&authSource=admin&retryWrites=true&w=majority`
-//     try{
-//     await mongoose.connect(url, { useNewUrlParser: true });
-//     console.log('database connected');
-//     }
-
-//     catch(error){
-//         console.log('error connecting',error)
-//     }
-
-// }
-
-// export default Connection
+const Data = async(username,password) => {
 
 
-import mongoose from "mongoose";
-
-
-
-const connection = async (uername, password) => {
-    // const URL = `mongodb://aditya:aditya12@cluster0-shard-00-00.jrlvl.mongodb.net:27017,cluster0-shard-00-01.jrlvl.mongodb.net:27017,cluster0-shard-00-02.jrlvl.mongodb.net:27017/?ssl=true&replicaSet=atlas-3prp21-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    // PaAHQhjX0ocBNUyH
-    const URL = `mongodb+srv://${uername}:${password}@cluster0.k2uq6xu.mongodb.net/?retryWrites=true&w=majority`;
-    // PaAHQhjX0ocBNUyH
-
-    try {
-        await mongoose.connect(URL, { useNewUrlParser: true });
-        console.log("database connected successfully");
-    } catch (error) {
-        
-        console.log(`Not Connected error is: ${error}`);
-
+    const Link = `mongodb://${username}:${password}@blog-website-shard-00-00.valyh.mongodb.net:27017,blog-website-shard-00-01.valyh.mongodb.net:27017,blog-website-shard-00-02.valyh.mongodb.net:27017/?ssl=true&replicaSet=atlas-2gwitb-shard-0&authSource=admin&retryWrites=true&w=majority`
+    try{
+    await mongoose.connect(Link, { useNewUrlParser: true });
+    console.log('database connected');
     }
+
+    catch(error){
+        console.log('error connecting',error)
+    }
+
 }
 
-export default connection;
-// module.exports=connection;
+export default Data
